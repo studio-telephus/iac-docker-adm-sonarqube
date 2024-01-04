05dba5ce847330346bc910af2eb91ebfd4b305d9
Kristo Aun <kristoaun@live.com>
2024-01-04 02:00:31 +0200

    s3

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "container_adm_sonarqube" {
-  source    = "github.com/studio-telephus/terraform-lxd-instance.git?ref=1.0.1"
+  source    = "github.com/studio-telephus/terraform-lxd-instance.git?ref=1.0.3"
   name      = "container-adm-sonarqube"
   image     = "images:debian/bookworm"
   profiles  = ["limits", "fs-dir", "nw-adm"]
@@ -19,10 +19,10 @@ module "container_adm_sonarqube" {
   exec_enabled = true
   exec         = "/mnt/install.sh"
   environment = {
-    RANDOM_STRING       = "1d9f2318-1f2d-4864-90b3-463a37801fff"
-    POSTGRES_PASSWORD = var.sonar_postgres_password
-    SONAR_OWNER_PASSWORD = var.sonar_owner_password
-    SONAR_USER_PASSWORD = var.sonar_user_password
+    RANDOM_STRING         = "1d9f2318-1f2d-4864-90b3-463a37801fff"
+    POSTGRES_PASSWORD     = var.sonar_postgres_password
+    SONAR_OWNER_PASSWORD  = var.sonar_owner_password
+    SONAR_USER_PASSWORD   = var.sonar_user_password
     SERVER_KEY_PASSPHRASE = var.platformrsa_key_passphrase
   }
 }
